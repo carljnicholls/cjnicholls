1bad8145a4eea07c1b3198653f328a0432999825
Carl J Nicholls <carljnicholls@hotmail.com>
2017-05-27 17:43:48 +0100

    express node.js server pug template engine mini.css framework

--- cjnicholls/routes/index.js
+++ cjnicholls/routes/index.js
@@ -1,26 +1,26 @@
-﻿/*
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
  * GET home page.
  */
-import express = require('express');
-const router = express.Router();
-
-router.get('/', (req: express.Request, res: express.Response) => {
+var express = require("express");
+var router = express.Router();
+router.get('/', function (req, res) {
     res.render('index', {
         title: 'CJNicholls',
         intro: 'Recently completed a BSc in Computing at Plymouth University. I have particular interests in bleeding edge technologies and current events within the technology domain. ',
         bio: 'winner'
     });
 });
-
-router.get('/contact', (req: express.Request, res: express.Response) => {
+router.get('/contact', function (req, res) {
     res.render('contact', {
         title: 'Contact',
         intro: 'Lorem ipsum dolor sit amet, mauris nibh eget nulla dolor dolor, ' +
             'pellentesque dignissim aliquam est leo urna, eu magna amet erat cubilia, ' +
             'sit eget amet augue, tellus vestibulum hendrerit amet nam eget.Sed enim tempor eget sed magna, ' +
-        'vel nunc tortor tincidunt ut, ac ad habitasse, urna cum integer libero faucibus.',
+            'vel nunc tortor tincidunt ut, ac ad habitasse, urna cum integer libero faucibus.',
         aboutSummary: ''
     });
 });
-
-export default router;
+exports.default = router;
+//# sourceMappingURL=index.js.map
