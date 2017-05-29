ebd075724ff691251e818552f43e483e0616fc35
Carl J Nicholls <carljnicholls@hotmail.com>
2017-05-29 14:07:56 +0100

    - reading list added - content tweaks

--- cjnicholls/routes/index.js
+++ cjnicholls/routes/index.js
@@ -9,8 +9,9 @@ router.get('/', function (req, res) {
     var contact = {
         Email: 'mailto:carljnicholls@hotmail.com',
         GitHub: 'https://github.com/carljnicholls',
-        LinkedIn: 'https://linkedin.com/in/carl-nicholls-0b8252144',
-        Résumé: '/pdf/cv.pdf'
+        LinkedIn: 'https://linkedin.com/in/carl-nicholls'
+        //,
+        //Résumé: '/pdf/cv.pdf'
     };
     res.render('index', {
         title: 'CJNicholls',
@@ -20,6 +21,25 @@ router.get('/', function (req, res) {
             'as well as the potential applications of these technologies.',
         sideElement: 'Contact:',
         contact: contact
+    });
+});
+router.get('/readinglist', function (req, res) {
+    var resources = {
+        'Practical Color Theory for People Who Code': 'https://tallys.github.io/color-theory/',
+        'Live CO2 emissions of Electricity Consumption': 'https://www.electricitymap.org/?wind=true&solar=true&page=map',
+        'Why is .NET reflection slow?': 'http://mattwarren.org/2016/12/14/Why-is-Reflection-slow/',
+        'Great. Now Even Your Headphones Can Spy on You': 'https://www.wired.com/2016/11/great-now-even-headphones-can-spy/',
+        'A collection of awesome .NET libraries, tools, frameworks, and software.': 'https://github.com/quozd/awesome-dotnet',
+        'A collection of awesome browser-side JavaScript libraries, resources and shiny things.': 'https://github.com/sorrycc/awesome-javascript',
+        'Apple Forces Recyclers to Shred All iPhones and MacBooks': 'https://motherboard.vice.com/en_us/article/apple-recycling-iphones-macbooks',
+        '': ''
+    };
+    res.render('reading', {
+        title: 'Reading List',
+        main: 'We all find books and resources that we feel should be shared due to their impact. ' +
+            'This may be due to how the content is presented to the user or the ' +
+            'subject matter. This is my own list.',
+        resources: resources
     });
 });
 //router.get('/contact', (req: express.Request, res: express.Response) => {
