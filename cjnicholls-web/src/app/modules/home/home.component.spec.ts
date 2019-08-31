604734a77b0f4db49110f536d64255a96b98f047
Carl <carljnicholls@hotmail.com>
2019-08-31 20:03:42 +0100

    Test Fixed

--- cjnicholls-web/src/app/modules/home/home.component.spec.ts
+++ cjnicholls-web/src/app/modules/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HomeBodyComponent } from './components/home-body/home-body.component';
 
 describe('HomeComponent', () => {
     let component: HomeComponent;
@@ -8,7 +9,12 @@ describe('HomeComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [HomeComponent]
+            declarations: [
+                HomeComponent,
+                HomeBodyComponent
+            ],
+            providers: [],
+            imports: []
         })
             .compileComponents();
     }));
